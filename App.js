4059a1c3b51ebfe79537f4c08856995aa0c2fdd3
DoanhLee <127416383+DoanhLee@users.noreply.github.com>
2023-06-04 17:04:07 +0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+function Car(props) {
+  return <h2>I am a { props.brand }!</h2>;
+}
+
+const myElement = <Car brand="Ford" />;
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(myElement);
+
+export default App;
